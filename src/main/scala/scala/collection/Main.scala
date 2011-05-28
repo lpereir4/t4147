a3683e7cc04063d31e2192c86b6b5e2ced6b79df
Lucien Pereira <pereira.lucien@laposte.net>
2011-05-28 10:30:42 +0200

    Updated benchmark code in order to add java.util.TreeSet curve.

--- src/main/scala/scala/collection/Main.scala
+++ src/main/scala/scala/collection/Main.scala
@@ -3,17 +3,21 @@ package scala.collection
 import mutable.{ AVLTree, Node, Leaf }
 
 object Main {
-  type Job[A] = Int => SortedSet[A] => Unit
-  type Builder[A] = () => SortedSet[A]
+  type Job[A] = Int => Set[A] => Unit
+  type JavaJob[A] = Int => java.util.TreeSet[A] => Unit
+  type Builder[A] = () => Set[A]
+  type JavaBuilder[A] = () => java.util.TreeSet[A]
 
   // builders
   val mutableSetBuilder: Builder[Int] = () => mutable.TreeSet()
 
   val immutableSetBuilder: Builder[Int] = () => immutable.TreeSet()
 
+  val javaMutableSetBuilder: JavaBuilder[Int] = () => new java.util.TreeSet[Int]()
+
   // chronometers
-  def measure[A]: Job[A] => Int => SortedSet[A] => Long = (job: Job[A]) => { (nb: Int) =>
-    { (s: SortedSet[A]) =>
+  def measure[A]: Job[A] => Int => Set[A] => Long = (job: Job[A]) => { (nb: Int) =>
+    { (s: Set[A]) =>
       {
         val start = System.nanoTime()
         job(nb)(s)
@@ -22,7 +26,30 @@ object Main {
     }
   }
 
-  val insertionRemovalJob: Job[Int] = (nb: Int) => { (s: SortedSet[Int]) =>
+  def javaMeasure[A]: JavaJob[A] => Int => java.util.TreeSet[A] => Long = (job: JavaJob[A]) => { (nb: Int) =>
+    { (s: java.util.TreeSet[A]) =>
+      {
+        val start = System.nanoTime()
+        job(nb)(s)
+        (System.nanoTime() - start) / 1000000
+      }
+    }
+  }
+
+  val javaInsertionRemovalJob : JavaJob[Int] = (nb: Int) => { (s: java.util.TreeSet[Int]) =>
+    {
+      val list = (1 to nb)
+      var d = s
+      for (i <- list) {
+        d.add(i)
+      }
+      for (i <- list) {
+        d.remove(i)
+      }
+    }
+  }
+
+  val insertionRemovalJob: Job[Int] = (nb: Int) => { (s: Set[Int]) =>
     {
       val list = (1 to nb)
       var d = s
@@ -35,32 +62,35 @@ object Main {
     }
   }
 
-  val sizeJob: Job[Int] = (nb: Int) => { (s: SortedSet[Int]) =>
+  val sizeJob: Job[Int] = (nb: Int) => { (s: Set[Int]) =>
     {
       val a = s.size
     }
   }
 
-  def insertionRemoval(n: Int): Seq[(Int, Long, Long)] = {
+  def insertionRemoval(n: Int): List[(Int, Long, Long, Long)] = {
     (for (j <- 1 to n) yield (j * 10000)).map(a =>
       (a,
+        measure(insertionRemovalJob)(a)(immutableSetBuilder()),
         measure(insertionRemovalJob)(a)(mutableSetBuilder()),
-        measure(insertionRemovalJob)(a)(immutableSetBuilder()))).toList
+        javaMeasure(javaInsertionRemovalJob)(a)(javaMutableSetBuilder())
+      )
+    ).toList
   }
 
+/*
   def size(n: Int): Seq[(Int, Long, Long)] = {
-    var s = mutableSetBuilder()
+    var s = mutableSortedSetBuilder()
     val view = s.rangeImpl(None, None)
     (1 to n).map(i => {
       s += i
       (i, measure(sizeJob)(0)(s), measure(sizeJob)(0)(view))
     })
-  }
+  }*/
 
-  def show(tab: Seq[(Int, Long, Long)]) {
-    println(tab.map(_._1).mkString(";"))
-    println(tab.map(_._2).mkString(";"))
-    println(tab.map(_._3).mkString(";"))
+  def show(tab: Seq[(Int, Long, Long, Long)]) {
+    println("size;immutable;mutable;java.mutable")
+    tab.foreach(line => println("%s;%s;%s;%s" format(line._1, line._2, line._3, line._4)))
   }
 
   def main(args: Array[String]) {
