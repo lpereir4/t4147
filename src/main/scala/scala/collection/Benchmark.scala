fa13ce6c3aca0cd199a325befb618e5a3790ccfc
Lucien Pereira <pereira.lucien@laposte.net>
2011-06-04 13:35:16 +0200

    benchmark code refactoring

--- src/main/scala/scala/collection/Benchmark.scala
+++ src/main/scala/scala/collection/Benchmark.scala
@@ -3,15 +3,20 @@ package scala.collection
 object Benchmark {
   type Job[A] = Seq[Int] => Set[A] => Unit
   type JavaJob[A] = Seq[Int] => java.util.TreeSet[A] => Unit
-  type Builder[A] = () => Set[A]
-  type JavaBuilder[A] = () => java.util.TreeSet[A]
+  type Builder[A] = Seq[A] => Set[A]
+  type JavaBuilder[A] = Seq[A] => java.util.TreeSet[A]
 
   // builders
-  val mutableSetBuilder: Builder[Int] = () => mutable.TreeSet[Int]()
+  val mutableSetBuilder: Builder[Int] = (seq: Seq[Int]) => mutable.TreeSet[Int](seq: _*)
 
-  val immutableSetBuilder: Builder[Int] = () => immutable.TreeSet[Int]()
+  val immutableSetBuilder: Builder[Int] = (seq: Seq[Int]) => immutable.TreeSet[Int](seq: _*)
 
-  val javaMutableSetBuilder: JavaBuilder[Int] = () => new java.util.TreeSet[Int]()
+  val javaMutableSetBuilder: JavaBuilder[Int] = (seq: Seq[Int]) => {
+    val a = new java.util.TreeSet[Int]()
+    for(i <- seq)
+      a.add(i)
+    a
+  }
 
   // chronometers
   def measure[A]: Job[A] => Seq[Int] => Set[A] => Long = (job: Job[A]) => { (seq: Seq[Int]) =>
@@ -34,74 +39,82 @@ object Benchmark {
     }
   }
 
-  val javaInsertionRemovalJob: JavaJob[Int] = (seq: Seq[Int]) => { (s: java.util.TreeSet[Int]) =>
+  val javaInsertionJob: JavaJob[Int] = (seq: Seq[Int]) => { (s: java.util.TreeSet[Int]) =>
     {
       var d = s
       for (i <- seq) {
         d.add(i)
       }
+    }
+  }
+
+  val javaRemovalJob: JavaJob[Int] = (seq: Seq[Int]) => { (s: java.util.TreeSet[Int]) =>
+    {
+      var d = s
       for (i <- seq) {
         d.remove(i)
       }
     }
   }
 
-  val insertionRemovalJob: Job[Int] = (seq: Seq[Int]) => { (s: Set[Int]) =>
+  val insertionJob: Job[Int] = (seq: Seq[Int]) => { (s: Set[Int]) =>
     {
       var d = s
       for (i <- seq) {
         d += i
       }
+    }
+  }
+
+  val removalJob: Job[Int] = (seq: Seq[Int]) => { (s: Set[Int]) =>
+    {
+      var d = s
       for (i <- seq) {
         d -= i
       }
     }
   }
 
-  def linearInsertionRemoval(n: Int): List[(Int, Long, Long, Long)] = {
-    val init = (1 to n*step).toSeq
-    (for (j <- 1 to n) yield (j * step)).map((a: Int) => {
-      val seq = init.take(a)
-      println(seq.size)
+  def regularData(size: Int): (String, Seq[Int]) = ("regularData", (1 to size))
+
+  def reverseData(size: Int): (String, Seq[Int]) = ("reverseData", regularData(size)._2.reverse)
+
+  def alternateData(size: Int): (String, Seq[Int]) = ("alternateData", regularData(size)
+						      ._2
+						      .map(x => if (x %2 == 0) -x else x).reverse)
+
+  def genericRemoval(n: Int, step: Int, f: (Int) => (String, Seq[Int])): (String, List[(Int, Long, Long, Long)]) = {
+    val data = f(step*n)
+    ("REMOVAL : "+data._1, (for (j <- 1 to n) yield (j * step)).map((a: Int) => {
+      val seq = data._2.take(a)
       (seq.size,
-        measure(insertionRemovalJob)(seq)(immutableSetBuilder()),
-        measure(insertionRemovalJob)(seq)(mutableSetBuilder()),
-        javaMeasure(javaInsertionRemovalJob)(seq)(javaMutableSetBuilder()))
-    }).toList
+        measure(insertionJob)(seq)(immutableSetBuilder(seq)),
+        measure(insertionJob)(seq)(mutableSetBuilder(seq)),
+        javaMeasure(javaInsertionJob)(seq)(javaMutableSetBuilder(seq)))
+    }).toList)
   }
 
-  val step = 10000
-
-  def experimentInsertionRemoval(n: Int): List[(Int, Long, Long, Long)] = {
-    val init = (1 to n*step).map(x => if (x %2 == 0) -x else x).toSeq.reverse
-    (for (j <- 1 to n) yield (j * step)).map(a => {
-      val seq = init.take(a)
-      println(seq.size)
+  def genericInsertion(n: Int, step: Int, f: (Int) => (String, Seq[Int])): (String, List[(Int, Long, Long, Long)]) = {
+    val data = f(step*n)
+    ("INSERTION : "+data._1, (for (j <- 1 to n) yield (j * step)).map((a: Int) => {
+      val seq = data._2.take(a)
       (seq.size,
-        measure(insertionRemovalJob)(seq)(immutableSetBuilder()),
-        measure(insertionRemovalJob)(seq)(mutableSetBuilder()),
-        javaMeasure(javaInsertionRemovalJob)(seq)(javaMutableSetBuilder()))
-    }).toList
+        measure(insertionJob)(seq)(immutableSetBuilder(Nil)),
+        measure(insertionJob)(seq)(mutableSetBuilder(Nil)),
+        javaMeasure(javaInsertionJob)(seq)(javaMutableSetBuilder(Nil)))
+    }).toList)
   }
 
-  def randomInsertionRemoval(n: Int): List[(Int, Long, Long, Long)] = {
-    val init = (1 to n*step).toSeq.reverse
-    (for (j <- 1 to n) yield (j * step)).map(a => {
-      val seq = init.take(a)
-      println(seq.size)
-      (seq.size,
-        measure(insertionRemovalJob)(seq)(immutableSetBuilder()),
-        measure(insertionRemovalJob)(seq)(mutableSetBuilder()),
-        javaMeasure(javaInsertionRemovalJob)(seq)(javaMutableSetBuilder()))
-    }).toList
-  }
-
-  def show(tab: Seq[(Int, Long, Long, Long)]) {
+  def show(tab: (String, Seq[(Int, Long, Long, Long)])) {
+    println(tab._1)
     println("#Cardinality scala.collection.immutable.TreeSet \"mutable TreeSet (based on an immutable AVL)\" java.util.TreeSet")
-    tab.foreach(line => println("%s %s %s %s" format (line._1, line._2, line._3, line._4)))
+    tab._2.foreach(line => println("%s %s %s %s" format (line._1, line._2, line._3, line._4)))
   }
 
   def main(args: Array[String]) {
-    show(experimentInsertionRemoval(args(0).toInt))
+    val step = 10000
+    show(genericInsertion(args(0).toInt, step , regularData))
+    println("---------------")
+    show(genericRemoval(args(0).toInt, step , regularData))
   }
 }
